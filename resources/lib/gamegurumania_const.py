9f835d0300c7ebaaa4c5091b0bffe0dc5ee95287
skipmodea1 <skipmodea1@outlook.com>
2022-05-22 16:44:49 +0200

    v1.0.8 (2022-04-22) leia branch (kodi 18)
    - using https instead of http
    - ignoring ssl error
    - leia branch (kodi 18)

--- resources/lib/gamegurumania_const.py
+++ resources/lib/gamegurumania_const.py
@@ -15,7 +15,7 @@ SETTINGS = xbmcaddon.Addon()
 LANGUAGE = SETTINGS.getLocalizedString
 IMAGES_PATH = os.path.join(xbmcaddon.Addon().getAddonInfo('path'), 'resources', 'images')
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-DATE = "2022-05-22"
+DATE = "2022-04-22"
 VERSION = "1.0.8"
 
 
